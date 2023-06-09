e718a57dabfc3c3ab06bc3be87203751852fe537
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-06-09 09:57:32 +0700

    bai thuc hanh dau tien

---
+++ src/main/java/com/example/practive_1/Practive1Application.java
@@ -0,0 +1,13 @@
+package com.example.practive_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practive1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practive1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practive_1/Practive1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practive_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practive1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
